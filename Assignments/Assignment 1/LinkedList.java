2e7997b1254ad921a8616c55e6b44ab6cbd6a187
u22658395 <u22658395@tuks.co.za>
2024-02-28 12:36:56 +0200

    completed the constructor, toString, and the append functions

--- Assignments/Assignment 1/LinkedList.java
+++ Assignments/Assignment 1/LinkedList.java
@@ -2,15 +2,33 @@ public class LinkedList {
     public CoordinateNode head;
 
     public LinkedList() {
+        head = null;
     }
 
     public LinkedList(int x, int y) {
+        head = new CoordinateNode(x, y);
     }
 
     public void append(int x, int y) {
+        if(head != null)
+        {
+            CoordinateNode newNode = new CoordinateNode(x,y);
+            CoordinateNode current = head;
+            appendHelper(current,newNode);
+        }
+        else{
+            head = new CoordinateNode(x, y);
+        }
     }
 
     public void appendList(LinkedList other) {
+        if(head == null)
+        {
+            head = other.head;
+        }
+        else{
+            appendListHelper(other, head);
+        }
     }
 
     public boolean contains(int x, int y) {
@@ -19,7 +37,10 @@ public class LinkedList {
 
     @Override
     public String toString() {
-        return "";
+        if( head == null){
+            return "Empty List";
+        }
+        return toStringHelper(head, "");
     }
 
     public int length() {
@@ -28,5 +49,38 @@ public class LinkedList {
 
     public LinkedList reversed() {
         return null;
+    }
+
+
+
+
+
+    //======Helper Functions=============Helper Functions=============Helper Functions=============Helper Functions=============Helper Functions=============Helper Functions=======
+    private void appendHelper(CoordinateNode current, CoordinateNode newNode){
+        if(current.next == null){
+            current.next = newNode;
+        }
+        else
+            appendHelper(current.next, newNode);
+    }
+    
+    private void appendListHelper(LinkedList list, CoordinateNode curr){
+        if(curr.next == null){
+            curr.next = list.head;
+        }
+        else
+        appendListHelper(list , curr.next);
+    }
+
+    private String toStringHelper(CoordinateNode node, String out) {
+        if(node.next == null){
+            return out+= node.toString();
+        }
+        else{
+            out+= node.toString() + " -> ";
+            return toStringHelper(node.next, out);
+        }
+        
+        
     }
 }
--- Assignments/Assignment 1/Main.java
+++ Assignments/Assignment 1/Main.java
@@ -1,8 +1,51 @@
 public class Main {
     // Output is in output.txt
     public static void main(String[] args) {
-        Maze m = new Maze("input.txt");
-        System.out.println(m.solve(3, 3, 1, 0));
-        System.out.println(m.validStarts(0, 0));
+        // Maze m = new Maze("input.txt");
+        // System.out.println(m.solve(3, 3, 1, 0));
+        // System.out.println(m.validStarts(0, 0));
+
+
+        testAppend();
+    }
+
+
+    private static void testAppend(){
+        System.out.println("=========Test Append functions============Test Append functions============Test Append functions============Test Append functions============");
+
+        LinkedList ll = new LinkedList();
+        System.out.println(ll);
+        ll.append(0, 1);
+        ll.append(3, 1);
+        System.out.println(ll +"\n");
+        
+
+        ll = new LinkedList(1,1);
+        System.out.println(ll);
+        ll.append(5, 2);
+        ll.append(3, 1);
+        System.out.println(ll +"\n");
+        
+        
+        System.out.println("=========Test appendList============Test appendList============Test appendList============Test appendList============");
+
+        LinkedList l2 = new LinkedList(10,10);
+        l2.append(100, 22);
+        l2.append(123, 90);
+        System.out.println("Linked List to append: " + l2 +"\n");
+        
+        ll = new LinkedList();
+        ll.append(5, 2);
+        ll.append(3, 1);
+        System.out.println("Linked List to append to: " + ll);
+        ll.appendList(l2);
+        System.out.println(ll + "\n");
+        
+        ll = new LinkedList(0,11);
+        ll.append(5, 2);
+        ll.append(3, 1);
+        System.out.println("Linked List to append to: " + ll);
+        ll.appendList(l2);
+        System.out.println(ll + "\n");
     }
 }
